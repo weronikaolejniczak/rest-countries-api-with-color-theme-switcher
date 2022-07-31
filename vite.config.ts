d992900ccc5c56317a80782053d6aa1b5a7fe862
Weronika Olejniczak <wer.olejniczak@gmail.com>
2022-07-31 15:51:59 +0200

    test: configure test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,4 +7,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  // TODO: fix TS error
+  // @ts-ignore
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
